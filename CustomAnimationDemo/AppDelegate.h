3c701e7be66049e04c206e86488acbdc6e748eaa
my <my@bogon>
2016-03-29 14:59:41 +0800

    Initial Commit

---
+++ CustomAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomAnimationDemo
+//
+//  Created by my on 16/3/29.
+//  Copyright © 2016年 base. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAnimationDemo
+//
+//  Created by my on 16/3/29.
+//  Copyright © 2016年 base. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
